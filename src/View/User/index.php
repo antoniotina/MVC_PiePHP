08a05ad37087be72850364508fa999ac9e350937
unknown <antonio.tina@epitech.eu>
2020-03-23 10:20:25 +0100

    Added login/index.php to test POST/GET/SERVER

---
+++ src/View/User/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index MVC_PiePHP</title>
+</head>
+<body>
+    <pre>
+        <?php
+
+            var_dump($_POST, $_GET, $_SERVER);
+
+        ?>
+    </pre>
+</body>
+</html>
