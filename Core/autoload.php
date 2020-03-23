ac434e34f574a258ecfb223d995be18d3e3a15a2
unknown <antonio.tina@epitech.eu>
2020-03-23 11:32:05 +0100

    Autoloader MK1

---
+++ Core/autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+spl_autoload_register("autoload");
+
+function autoload($classname)
+{
+    static $counter = 0;
+    $path = ["./src/Model/", "./src/View", "./src/Controller/", "./Core/"];
+    $extension = ".php";
+
+    // Path of the counter, which goes through each folder one by one recursively. Exploding the subnamespaces/namespaces away to get the class. And the php extension.
+    $fullPath = $path[$counter] . explode("\\", $classname)[sizeof(explode("\\", $classname))-1] . $extension;
+
+    if (!file_exists($fullPath)) {
+        if ($counter <= sizeof($path)) {
+            $counter++;
+            autoload($classname);
+        }
+        return false;
+    }
+    
+    include_once $fullPath;
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index MVC_PiePHP</title>
+</head>
+<body>
+    <pre>
+        <?php
+
+            var_dump($_POST, $_GET, $_SERVER);
+
+            define("BASE_URI", str_replace("\\", "/", substr(__DIR__, strlen($_SERVER["DOCUMENT_ROOT"]))));
+            require_once(implode(DIRECTORY_SEPARATOR, ["Core", "autoload.php"]));
+            $app = new Core\Core();
+            $app->run();
+
+        ?>
+    </pre>
+</body>
+</html>
