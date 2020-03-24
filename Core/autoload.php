71836179b7d5811352d33df3a1d180b3a5845179
unknown <antonio.tina@epitech.eu>
2020-03-24 11:14:15 +0100

    Converted autoloader into class. Added all other files

--- Core/autoload.php
+++ Core/autoload.php
@@ -1,23 +1,34 @@
 <?php
 
-spl_autoload_register("autoload");
+namespace Core;
 
-function autoload($classname)
+class Autoloader
 {
-    static $counter = 0;
-    $path = ["./src/Model/", "./src/View", "./src/Controller/", "./Core/"];
-    $extension = ".php";
+    public function autoload($classname)
+    {
+        static $counter = 0;
+        $path = ["./src/Model/", "./src/View", "./src/Controller/", "./Core/"];
+        $extension = ".php";
 
-    // Path of the counter, which goes through each folder one by one recursively. Exploding the subnamespaces/namespaces away to get the class. And the php extension.
-    $fullPath = $path[$counter] . explode("\\", $classname)[sizeof(explode("\\", $classname))-1] . $extension;
+        // Path of the counter, which goes through each folder one by one recursively. Exploding the subnamespaces/namespaces away to get the class. And the php extension.
+        $fullPath = $path[$counter] . explode("\\", $classname)[sizeof(explode("\\", $classname)) - 1] . $extension;
 
-    if (!file_exists($fullPath)) {
-        if ($counter <= sizeof($path)) {
-            $counter++;
-            autoload($classname);
+        if (!file_exists($fullPath)) {
+            if ($counter <= sizeof($path)) {
+                $counter++;
+                $this->autoload($classname);
+            }
+            return false;
         }
-        return false;
+
+        include_once $fullPath;
     }
-    
-    include_once $fullPath;
+
+    public function launchAutoloader()
+    {
+        spl_autoload_register(array(__CLASS__, "autoload"));
+    }
 }
+
+$autoLoader = new Autoloader();
+$autoLoader->launchAutoloader();
