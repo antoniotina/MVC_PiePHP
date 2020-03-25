280d28a0e7f5f13fbf61a87ab7f210ae28b5fa48
unknown <antonio.tina@epitech.eu>
2020-03-25 16:33:19 +0100

    Final bug fixes

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
             var_dump($_POST, $_GET, $_SERVER);
 
-            define("BASE_URI", str_replace("\\", "/", substr(__DIR__, strlen($_SERVER["DOCUMENT_ROOT"]))));
+            define("BASE_URI", str_replace(DIRECTORY_SEPARATOR, "/", substr(__DIR__, strlen($_SERVER["DOCUMENT_ROOT"]))));
             require_once(implode(DIRECTORY_SEPARATOR, ["Core", "autoload.php"]));
             $app = new Core\Core();
             $app->run();
