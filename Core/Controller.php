6085e05c96293ab22893a7c578d2febe2c0a068b
unknown <antonio.tina@epitech.eu>
2020-03-27 19:03:48 +0100

    Added render in Core\Controller

--- Core/Controller.php
+++ Core/Controller.php
@@ -4,7 +4,24 @@ namespace Core;
 
 class Controller
 {
+    private $_render;
 
+    protected function render($view, $scope = [])
+    {
+        extract($scope);
+        $f = implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'src ', 'View ', str_replace('
+        Controller ', '', basename(get_class($this))), $view]) . '. php ';
+        if (file_exists($f)) {
+            ob_start();
+            include($f);
+            $view = ob_get_clean();
+            ob_start();
+            include(implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'src ', 'View ', 'index']) . '. php ');
+            self::$_render = ob_get_clean();
+        }
+        return self::$_render;
+    }
+    // protected function render in PDF
 
 
     // public function testMe()
